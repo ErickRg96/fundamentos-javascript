82da8249f41e2f95add0811812a218417de92557
Erick Reyes <erguzman96@gmail.com>
2020-04-20 12:23:15 -0500

    Primer avance

---
+++ 1-Primeros-pasos/01-variables.js
@@ -0,0 +1,15 @@
+// // Declaracion
+// var nombre;
+
+// // Inicializacion
+// nombre = 'Erick';
+
+// Declaracion e inicializacion
+// var nombre = 'Erick';
+// var apellido = 'Reyes';
+var nombre = 'Erick', apellido = 'Reyes';
+var edad = 28
+
+console.log('Hola ' + nombre + ' ' + apellido);
+console.log('Tengo ' + edad + ' años');
+
---
+++ 1-Primeros-pasos/02-variables-string.js
@@ -0,0 +1,15 @@
+var nombre = 'Erick', apellido = 'Reyes'
+
+var nombreMayusculas = nombre.toUpperCase();
+
+var apellidoMinusculas = apellido.toLowerCase();
+
+var primeraLetraNombre = nombre.charAt(4);
+
+var cantidadLetras = nombre.length;
+
+var nombreCompleto = `Mi nombre es ${nombre.toUpperCase()} ${apellido}`
+
+var str = nombre.substr(1,2)
+
+var ultimaLetra = nombre.charAt(nombre.length-1)
